4413993f63f5b41d17232f65a9ca737cc0644ed9
Garrett Lockhart <lockhartphotography1@gmail.com>
2022-08-30 23:26:09 -0600

    feat: use modular routes

--- server.js
+++ server.js
@@ -7,48 +7,15 @@ const PORT = process.env.PORT || 3002;
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const notesRouter = require('./routes/notesRoute');
 app.use('/notes', notesRouter);
 
-app.get('/api/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './db/db.json'));
-});
-
-app.post('/api/notes', (req, res) => {
-  const { title, text } = req.body;
-
-  const newNote = {
-    title,
-    text,
-    'uuid': uuidv4()
-  };
-
-  fs.readFile('./db/db.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    const parsedNotes = JSON.parse(data);
-    parsedNotes.push(newNote);
-
-    fs.writeFile(
-      './db/db.json',
-      JSON.stringify(parsedNotes, null, 2),
-      (err) => {
-        err ? console.error(err) : console.info('Successfully wrote db.json');
-      }
-    );
-  });
-});
+const apiRouter = require('./routes/apiRoutes');
+app.use('/api', apiRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
